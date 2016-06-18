a9ffbab4066f75bf8e1a8ac4535c895c48dd674d
Christopher Mlalazi <chrismlalazi@gmail.com>
2016-06-18 18:18:10 -0500

    Implemented time seek slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-var vid, playbtn, seekslider;
+//Make the play/pause button work in the video
+//Create the seeksleder which shows runing time of video
+
+var vid, playbtn, seekslider, curtimetext, durtimetext;
 
 function initializePlayer () {
 	//Set object references
 	vid = document.getElementById("my_video");
 	playbtn = document.getElementById("playpausebtn");
 	seekslider = document.getElementById("seekslider");
+	curtimetext = document.getElementById("curtimetext")
+	durtimetext  = document.getElementById("durtimetext")
 	//add event listemners
 	playbtn.addEventListener("click", playPause, false);
 	seekslider.addEventListener("change", vidSeek, false);
@@ -13,6 +18,7 @@ function initializePlayer () {
 
 window.onload = initializePlayer;
 
+//Activate play/pause button
 function playPause(){
 	if(vid.paused){
 		vid.play();
@@ -23,6 +29,7 @@ function playPause(){
 	}
 }
 
+//Video suration on seekslider
 function vidSeek() {
    var seekto = vid.duration * (seekslider.value / 100);
    vid.currentTime = seekto;
@@ -33,4 +40,30 @@ function seektimeupdate() {
  		var nt = vid.currentTime * (100 / vid.duration);
  		seekslider.value = nt;
 
+ 		//Time indicator on seekslider
+ 		var curmins = Math.floor(vid.currentTime / 60);
+		var cursecs = Math.floor(vid.currentTime - curmins * 60);
+		var durmins = Math.floor(vid.duration / 60);
+		var dursecs = Math.floor(vid.duration - durmins * 60);
+		if(cursecs < 10){ cursecs = "0"+cursecs; }
+		if(dursecs < 10){ dursecs = "0"+dursecs; }
+		if(curmins < 10){ curmins = "0"+curmins; }
+		if(durmins < 10){ durmins = "0"+durmins; }
+		curtimetext.innerHTML = curmins+":"+cursecs;
+		durtimetext.innerHTML = durmins+":"+dursecs;
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
